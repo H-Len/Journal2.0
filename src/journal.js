6d4a463d38b4ed1df9d69131db3202d8074de8dc
Daniel Hongel and Tae Lee <tae@>
2019-06-24 16:39:08 -0700

    This is part of word practice with webpack.

--- src/journal.js
+++ src/journal.js
@@ -6,6 +6,14 @@ export function myJournal(title, body) {
 myJournal.prototype.countWord = function () {
   var numberTitle = this.title.split(' ').length;
   var numberBody = this.body.split(' ').length;
-  console.log(numberTitle + numberBody);
+  // console.log(numberTitle + numberBody);
   return numberTitle + numberBody;
+};
+
+myJournal.prototype.countVowel = function () {
+  var vowels = ['a', 'e', 'i', 'o', 'u']
+  var vowelTitle = this.title.split(' ');
+  var vowelBody = this.body.split(' ');
+  console.log(vowelTitle);
+
 };
